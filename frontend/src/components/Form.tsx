50dcb3e05cbaa7bde33efe8430a2adabbf8a7f59
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-06-26 18:09:43 -0300

    Adjust event responsive

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -32,31 +32,36 @@ export function Form(){
     } 
 
     return (
-        <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-            <input 
-                className="bg-gray-900 rounded px-5 h-14"
-                type="text" 
-                placeholder="Seu nome completo"
-                onChange={event => setName(event.target.value)}
-                value={name}
-            />
-            <input 
-                className="bg-gray-900 rounded px-5 h-14"
-                type="email" 
-                placeholder="Digite seu e-mail"
-                onChange={event => setEmail(event.target.value)}
-                value={email}
-            />
-            {errorMessage !== "" && (
-                <p className="text-red-700">{errorMessage}</p>
-            )}
-            <button
-                type="submit"
-                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
-                disabled ={loading}
-            >
-                Garantir minha vaga
-            </button>
-        </form>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+            <strong className="text-2xl mb-6 block"> 
+                Inscreva-se gratuitamente 
+            </strong>
+            <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                <input 
+                    className="bg-gray-900 rounded px-5 h-14"
+                    type="text" 
+                    placeholder="Seu nome completo"
+                    onChange={event => setName(event.target.value)}
+                    value={name}
+                />
+                <input 
+                    className="bg-gray-900 rounded px-5 h-14"
+                    type="email" 
+                    placeholder="Digite seu e-mail"
+                    onChange={event => setEmail(event.target.value)}
+                    value={email}
+                />
+                {errorMessage !== "" && (
+                    <p className="text-red-700">{errorMessage}</p>
+                )}
+                <button
+                    type="submit"
+                    className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+                    disabled ={loading}
+                >
+                    Garantir minha vaga
+                </button>
+            </form>
+        </div>
     )
 }
--- frontend/src/pages/Subscribe.tsx
+++ frontend/src/pages/Subscribe.tsx
@@ -6,28 +6,26 @@ import codeMockupImg from '/src/assets/code-mockup.png'
 export function Subscribe(){ 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+            <div className="w-full max-w-[1100px] flex lg:flex-row flex-col items-center justify-between mt-20 mx-auto">
 
                 <div className="max-w-[640px]">
-                    <Logo/>
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
-                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
+                    <div className="flex lg:justify-start justify-center"><Logo/></div>
+                    <h1 className="mt-8 lg:text-[2.5rem] text-3xl lg:text-left  text-center leading-tight ml-1 mr-1">
+                        Construa uma 
+                        <strong className="text-blue-500"> aplicação completa</strong>
+                        , do zero, com 
+                        <strong className="text-blue-500"> React</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed lg:text-left text-center ml-1 mr-1">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas 
                         e com alta demanda para acessar as melhores oportunidades do mercado
                     </p>
                 </div>
 
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-                    <strong className="text-2xl mb-6 block"> 
-                        Inscreva-se gratuitamente 
-                    </strong>
+                <div className="mt-8">
                     <Form/>
                 </div>
-
             </div>
-
             <img src={codeMockupImg} alt="img" className="mt-10"/>
         </div>
     )
